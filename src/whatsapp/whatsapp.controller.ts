1838a58c573b86108ff6baea65ec85e2c0b6c889
unknown <musicadwigurniwa@gmail.com>
2024-08-31 04:05:21 +0700

    add send message to group

--- src/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { WhatsappService } from './whatsapp.service';
+import * as dotenv from 'dotenv';
 
 @Controller('whatsapp')
 export class WhatsappController {
-  constructor(private readonly whatsappService: WhatsappService) {}
+  constructor(private readonly whatsappService: WhatsappService) { }
 
   @Get('status')
   getStatus(): string {
@@ -11,7 +12,7 @@ export class WhatsappController {
   }
 
   @Post('send-message')
-  async sendMessage(@Body() body: { number: string; message: string; apikey: string}) {
+  async sendMessage(@Body() body: { number: string; message: string; apikey: string }) {
     const { number, message, apikey } = body;
     return await this.whatsappService.sendMessage(number, message, apikey);
   }
@@ -21,4 +22,20 @@ export class WhatsappController {
     const { number, imageUrl, caption, apikey } = body;
     return await this.whatsappService.sendImage(number, imageUrl, caption, apikey);
   }
+
+  @Get('groups')
+  async getGroups() {
+    const groups = await this.whatsappService.getAllGroups();
+    return groups;
+  }
+
+  @Post('send-to-group')
+    async sendMessageToGroup(
+        @Body('groupNameOrId') groupNameOrId: string,
+        @Body('message') message: string,
+        @Body('apikey') apikey: string,
+    ) {
+        await this.whatsappService.sendMessageToGroup(groupNameOrId, message, apikey);
+        return { status: 'Message sent to group' };
+    }
 }
--- src/whatsapp/whatsapp.service.ts
+++ src/whatsapp/whatsapp.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Client, LocalAuth, MessageMedia } from 'whatsapp-web.js';
+import { Client, LocalAuth, MessageMedia, Chat, Message } from 'whatsapp-web.js';
 import axios from 'axios';
 import * as qrcode from 'qrcode-terminal';
 import * as dotenv from 'dotenv';
@@ -27,25 +27,25 @@ export class WhatsappService {
     });
 
     this.client.on('qr', (qr) => {
-        qrcode.generate(qr, { small: true });
+      qrcode.generate(qr, { small: true });
     });
 
     this.client.on('ready', () => {
-        console.log('WhatsApp bot is ready!');
-      });
+      console.log('WhatsApp bot is ready!');
+    });
 
     this.client.on('authenticated', () => {
-        console.log('WhatsApp authenticated!');
-      });
-    
+      console.log('WhatsApp authenticated!');
+    });
+
     this.client.on('auth_failure', msg => {
-        console.error('Authentication failure:', msg);
-      });
+      console.error('Authentication failure:', msg);
+    });
 
     this.client.on('disconnected', (reason) => {
-        console.log('WhatsApp disconnected:', reason);
-        this.client.initialize(); // Re-initialize the client
-      });
+      console.log('WhatsApp disconnected:', reason);
+      this.client.initialize(); // Re-initialize the client
+    });
 
     this.client.initialize();
   }
@@ -54,17 +54,18 @@ export class WhatsappService {
     //await this.client.initialize(); // Ensure client is initialize
     const validApiKey = process.env.API_KEY; // Mengambil API key dari variabel lingkungan
     if (apikey == validApiKey) {
-    const chatId = `${number}@c.us`; // Format nomor telepon untuk WhatsApp
-    try {
-      await this.client.sendMessage(chatId, message);
-      return { status: 'success', message: `Message sent to ${number}` };
-    } catch (error) {
-      console.error('Error sending message:', error);
-      return { status: 'error', message: `Failed to send message to ${number}` };
-    }}
+      const chatId = `${number}@c.us`; // Format nomor telepon untuk WhatsApp
+      try {
+        await this.client.sendMessage(chatId, message);
+        return { status: 'success', message: `Message sent to ${number}` };
+      } catch (error) {
+        console.error('Error sending message:', error);
+        return { status: 'error', message: `Failed to send message to ${number}` };
+      }
+    }
 
     else {
-        return 'salah';
+      return 'salah';
     }
   }
 
@@ -72,21 +73,50 @@ export class WhatsappService {
     //await this.client.initialize(); // Ensure client is initialize
     const validApiKey = process.env.API_KEY; // Mengambil API key dari variabel lingkungan
     if (apikey == validApiKey) {
-    const chatId = `${number}@c.us`;
-    try {
-      // Download image
-      const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
-      const media = new MessageMedia('image/jpeg', Buffer.from(response.data).toString('base64'));
+      const chatId = `${number}@c.us`;
+      try {
+        // Download image
+        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        const media = new MessageMedia('image/jpeg', Buffer.from(response.data).toString('base64'));
 
-      // Send image with caption
-      await this.client.sendMessage(chatId, media, { caption });
-      return { status: 'success', message: `Image sent to ${number}` };
-    } catch (error) {
-      console.error('Error sending image:', error);
-      return { status: 'error', message: `Failed to send image to ${number}` };
-    }}
+        // Send image with caption
+        await this.client.sendMessage(chatId, media, { caption });
+        return { status: 'success', message: `Image sent to ${number}` };
+      } catch (error) {
+        console.error('Error sending image:', error);
+        return { status: 'error', message: `Failed to send image to ${number}` };
+      }
+    }
 
     else {
-        return 'salah';
-    }}
+      return 'salah';
+    }
+  }
+
+  async getAllGroups(): Promise<{ name: string; id: string }[]> {
+    const chats = await this.client.getChats();
+    const groups = chats
+      .filter(chat => chat.isGroup)
+      .map((group: Chat) => ({
+        name: group.name,
+        id: group.id._serialized,
+      }));
+
+    return groups;
+  }
+
+  // Fungsi untuk mengirim pesan ke grup berdasarkan nama atau ID grup
+  async sendMessageToGroup(groupNameOrId: string, message: string, apikey: string): Promise<Message | void> {
+    const validApiKey = process.env.API_KEY; // Mengambil API key dari variabel lingkungan
+    if (apikey == validApiKey) {
+      const chats = await this.client.getChats();
+      const group = chats.find(chat => chat.isGroup && (chat.name === groupNameOrId || chat.id._serialized === groupNameOrId));
+
+      if (!group) {
+        throw new Error(`Group with name or ID "${groupNameOrId}" not found`);
+      }
+
+      return await this.client.sendMessage(group.id._serialized, message);
+    }
+  }
 }
